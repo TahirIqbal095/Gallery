b97308e7b9f221bb0d793a714db35345410272cf
Tahir iqbal <shahtahir786@gmail.com>
2023-03-08 23:29:11 +0530

    added navbar and animation to it

--- build/app.js
+++ build/app.js
@@ -5,3 +5,16 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5})
 tl.to(".intro", { y: "-100%", duration: 1}, "<")
 tl.fromTo("nav", { opacity:0}, {opacity:1, duration: 1})
 tl.fromTo(".welcome-msg", { opacity:0}, {opacity:1, duration: 1}, "<")
+
+// navbar
+
+const hambrgerBtn = document.querySelector(".hamburger")
+const hamburgerItems = document.querySelector(".hamburger-items")
+const hamburgerIcon = document.querySelector(".hamburger-icon")
+const hamburgerCloseIcon = document.querySelector(".hamburger-close-icon")
+
+hambrgerBtn.addEventListener("click", () =>{
+    hamburgerItems.classList.toggle("open")
+    hamburgerIcon.classList.toggle("hidden")
+    hamburgerCloseIcon.classList.toggle("hidden")
+})
